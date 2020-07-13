4d35a7f67251ff139c3eca1d4ebddb45cfc405d7
violetfocus <violetfocus0618@gmail.com>
2020-07-13 12:32:53 +0300

    pagination added
    pagination added

--- resources/views/admin/tracing/check_in_logs.blade.php
+++ resources/views/admin/tracing/check_in_logs.blade.php
@@ -1,8 +1,8 @@
 @include('layouts.header')
         <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css"/>
-        <script type="text/javascript" src="//cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
+        <!-- <script type="text/javascript" src="//cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script> -->
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"/></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"/></script>
+        <script type="text/javascript" src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"/></script>
         <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"/></script>
         <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"/></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"/></script>
